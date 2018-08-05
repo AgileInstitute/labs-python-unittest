f3c56f6ebc09d4b59d30543c4585650697298026
Rob Myers <Rob.Myers@agileInstitute.com>
2018-08-05 13:06:27 -0700

    Removing a backup that didn't need a commit

--- PriorTestedTrek/tests/mocks/MockRandom.py
+++
@@ -1,8 +0,0 @@
-
-from trek.Random import Random
-
-
-class MockRandom(Random):
-    def Next(self, maxValue):
-        # always return 1/2 max:  It isn't random, and that's the point!
-        return maxValue / 2
