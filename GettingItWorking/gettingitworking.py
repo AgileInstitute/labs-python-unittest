2969883933894ff13aac4f99aa01a5737326e89a
Rob Myers <Rob.Myers@agileInstitute.com>
2018-08-19 11:47:47 -0700

    broke 1 test into 2
    and added a class to be tested

--- GettingItWorking/gettingitworking.py
+++ GettingItWorking/gettingitworking.py
@@ -1,13 +1,22 @@
 import unittest
- 
-class MyTest(unittest.TestCase):
- 
-    def test_square(self):
-	self.assertEqual(4, square(2))
-	self.assertEqual(9, square(3))
 
-def square(x):
-    return x + x
+
+class MyTest(unittest.TestCase):
+
+    def setUp(self):
+        self.my_maths = MyMaths()
+ 
+    def test_squares_two(self):
+        self.assertEqual(4, self.my_maths.square(2))
+
+    def test_squares_three(self):
+        self.assertEqual(9, self.my_maths.square(3))
+
+class MyMaths:
+    def square(self, x):
+        return x + x
+
+
 
 if __name__ == '__main__':
     unittest.main()
