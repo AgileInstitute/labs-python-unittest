e7f8104a2c493a7b63ad4ebc85200ae2ee7162d6
Rob Myers <Rob.Myers@agileInstitute.com>
2018-08-23 11:14:58 -0700

    WARNING: I'm intentionally checking in a FAILING TEST! (fix left to the reader ;-) Using more MagicMock functionality to test number of times mock is called.

--- LunEx/test_stock_quote.py
+++ LunEx/test_stock_quote.py
@@ -17,7 +17,7 @@ class StockQuote:
 
             return int(self.number_shares
                        * self.exchange_service.current_price(self.symbol)
-                       * 1.02) + 10;
+                       * 1.02) + 10
         except LunExServiceUnavailableException as e:
             raise EarthboundError(e)
 
@@ -30,23 +30,26 @@ class StockQuoteTest(unittest.TestCase):
 
     def ignored_test_quote_total_includes_fees(self):
         service = LunExServices();
-        quote = StockQuote("HE3", 100, service);
+        quote = StockQuote("HE3", 100, service)
         total = quote.total()
         self.assertEqual(1234, total)
 
     def test_quote_total_includes_fees(self):
-        service = LunExServices();
-        service.current_price = MagicMock(return_value=12)
-        quote = StockQuote("HE3", 100, service);
+        service = LunExServices()
+        mock_function = MagicMock(return_value=12)
+        service.current_price = mock_function
+        quote = StockQuote("HE3", 100, service)
 
         total = quote.total()
 
         self.assertEqual(1234, total)
+        quote.total() # calling again to assure immutability
+        mock_function.assert_called_once()
 
     def test_earthbound_exception_raised_on_sunspots(self):
-        service = LunExServices();
+        service = LunExServices()
         service.current_price = MagicMock(side_effect= \
                       LunExServiceUnavailableException("sunspots"))
-        quote = StockQuote("HE3", 100, service);
+        quote = StockQuote("HE3", 100, service)
 
         self.assertRaises(EarthboundError, quote.total)
